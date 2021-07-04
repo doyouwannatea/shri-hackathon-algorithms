0b0a8e5e7bbbb230c65fb62f3b0e06d7f2151850
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2021-07-04 16:14:41 +0300

    Сортировка вставками

--- insertSorting.js
+++ insertSorting.js
@@ -1,4 +1,5 @@
 const list = [-13, -33, 2, -22, 220, 2, 1, 1] // => [-33, -22, -13, 1, 1, 2, 2, 220]
+// const list = [-13, 2, -33] // => [-33, -13, 2]
 
 function insertSorting(list = []) {
     const arr = [...list]
@@ -6,11 +7,19 @@ function insertSorting(list = []) {
     for (let i = 1; i < arr.length; i++) {
         const currEl = arr[i]
 
-        for (let j = i; j > 0; j--) {
+        for (let j = i; j > -1; j--) {
+
+            if (j - 1 < 0) {
+                arr[j] = currEl
+            }
+
             if (currEl < arr[j - 1]) {
                 arr[j] = arr[j - 1]
-                arr[j - 1] = currEl
+            } else {
+                arr[j] = currEl
+                break
             }
+
         }
     }
 
